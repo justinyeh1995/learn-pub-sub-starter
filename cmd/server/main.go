33c6bf1c14b53a3f3dec57ba6dc5a311a4126b4e
justin21013 <justin@cacafly.com>
2025-06-28 16:23:46 +0800

    ch2-5 done

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "log"
+    "os"
+    "os/signal"
+    amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+    fmt.Println("Starting Peril server...")
+    const connStr := "amqp://guest:guest@localhost:5672/"
+    conn, err := amqp.Dial(connStr)
+    if (err != nil) {
+        log.Panicf("Unable to establish a connection.")
+    }
+    
+    defer conn.Close()
+    fmt.Println("Connection Succeeds")
+    
+    // How do you wait for a signal in go?
+    signalChan := make(chan os.Signal, 1)
+    signal.Notify(signalChan, os.Interrupt)
+    
+    // Block until a signal is received. 
+    s := <-signalChan
+    fmt.Println("Got signal:", s)
 }
