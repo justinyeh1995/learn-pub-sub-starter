e5591045394f2f3b4b9a4d02b301b157bb87f5a1
justin21013 <justin@cacafly.com>
2025-07-25 12:02:31 +0800

    update to justinyeh1995

--- internal/gamelogic/logs.go
+++ internal/gamelogic/logs.go
@@ -6,7 +6,7 @@ import (
 	"os"
 	"time"
 
-	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	"github.com/justinyeh1995/learn-pub-sub-starter/internal/routing"
 )
 
 const logsFile = "game.log"
