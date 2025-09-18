6737f53c27278f25232d35916eee9175bdc8d5c1
justinyeh1995 <justinyeh1995@gmail.com>
2025-09-18 17:56:48 +0800

    [Enhance][Pubsub] Update DeclareAndBind function to pass an amqp.Table to the QueueDeclare function that includes a x-dead-letter-exchange key. The value is the name of your dead letter exchange

--- internal/pubsub/publish.go
+++ internal/pubsub/publish.go
@@ -54,7 +54,9 @@ func DeclareAndBind(
 		queueType == Transient,
 		queueType == Transient,
 		false,
-		nil)
+		amqp.Table{
+			"x-dead-letter-exchange": "peril_dlx",
+		})
 	if err != nil {
 		return nil, amqp.Queue{}, fmt.Errorf("%w", err)
 	}
